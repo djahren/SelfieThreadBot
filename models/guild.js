348416efa8c731528236f6daec5bdd11d5f31264
djahren <djahren@djahren.com>
2022-09-14 22:41:45 -0700

    formatting

--- models/guild.js
+++ models/guild.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const guildSchema = new mongoose.Schema({
-    guildId: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: false
-    },
-    channels:{
-        type: Array,
-        required: false
-    },
-    autoArchiveDuration:{
-        type: Number,
-        required: true,
-        default: 1440
-    }
-})
+	guildId: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+		required: false,
+	},
+	channels:{
+		type: Array,
+		required: false,
+	},
+	autoArchiveDuration:{
+		type: Number,
+		required: true,
+		default: 1440,
+	},
+});
 
-module.exports = mongoose.model('Guild', guildSchema)
+module.exports = mongoose.model('Guild', guildSchema);
